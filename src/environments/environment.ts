39322f3d4d78808d488b5c4dd9eb7cbe64cce940
totodore <prevottheodore@gmail.com>
2021-03-13 20:16:00 +0100

    chanching env for non prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: "doscenario.scriptis.fr",
+  production: false,
+  apiUrl: "localhost:3000",
 };
 
 /*

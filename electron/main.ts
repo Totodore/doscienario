9a3c9dbd5bd4e2e29cb3bc23293343c3605477f4
Totodore <prevottheodore@gmail.com>
2021-09-26 19:11:44 +0200

    building electron version directly from this repo

---
+++ electron/main.ts
@@ -0,0 +1,40 @@
+import { app as electron, BrowserWindow, shell } from "electron";
+import { globalShortcut } from "electron/main";
+
+class App {
+  private readonly url = `dist/app/index.html`;
+  // private readonly url = `./app/index.html`;
+  private window: BrowserWindow;
+
+  public async init(): Promise<void> {
+    await new Promise<void>(resolve => electron.on("ready", () => resolve()));
+    this.window = new BrowserWindow({
+      maximizable: true,
+      icon: "../icons/icon.ico",
+      titleBarStyle: 'hidden'
+    });
+    this.config();
+    // await this.window.loadURL("http://localhost:4200/");
+    await this.window.loadFile(this.url);
+  }
+
+  public config() {
+    this.window.webContents.on("will-navigate", (e, url) => {
+      if (!url.startsWith("file://"))
+        shell.openExternal(url);
+    });
+    this.window.setMenu(null);
+    this.window.menuBarVisible = false;
+    this.window.on('closed', () => {
+      this.window = null;
+    });
+    globalShortcut.register("CmdOrCtrl+I", () => this.window.webContents.openDevTools());
+    electron.on('window-all-closed', () => {
+      // On macOS it is common for applications to stay open
+      // until the user explicitly quits
+      if (process.platform !== 'darwin') electron.quit()
+    });
+  }
+}
+
+new App().init();

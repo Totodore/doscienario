6df41509929dac41dd7d8f55ef8277aab97998a6
Totodore <prevottheodore@gmail.com>
2021-10-17 22:18:47 +0200

    removing logs

--- src/app/decorators/data-updater.decorator.ts
+++ src/app/decorators/data-updater.decorator.ts
@@ -4,7 +4,6 @@ export function DataUpdater() {
     const original = descriptor.value;
     descriptor.value = function (...args: any[]) {
       const result = original.apply(this, args);
-      console.log(this);
       if (result instanceof Promise) {
         result.then(() => {
           this.saveData();

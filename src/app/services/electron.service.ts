84285b749a2370000c09234ff4ba130458c131f8
Totodore <prevottheodore@gmail.com>
2022-01-04 22:07:37 +0100

    patching is electron app

--- src/app/services/electron.service.ts
+++ src/app/services/electron.service.ts
@@ -23,7 +23,22 @@ export class ElectronService {
   }
 
   public get isElectronApp(): boolean {
-    return isElectron();
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+      return true;
+    }
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+      return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to false
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+      return true;
+    }
+
+    return false;
   }
 
   public send(channel: string, ...args: any[]) {

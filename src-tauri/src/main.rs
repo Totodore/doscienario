5888827c76d12371b1c303ca7cb8f27bd17c8a0b
theodore <prevottheodore@gmail.com>
2024-11-13 02:32:32 +0100

    update to tauri v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,25 @@
 #![cfg_attr(
-	all(not(debug_assertions), target_os = "windows"),
-	windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 use tauri::Manager;
 
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
-	// Close splashscreen
-	if let Some(splashscreen) = window.get_window("splashscreen") {
-		println!("Application ready, closing splashscreen");
-		splashscreen.close().unwrap();
-	}
-	let main_window = window.get_window("main").unwrap();
-	// Show main window
-	main_window.show().unwrap();
-	main_window.set_focus().unwrap();
+    // Close splashscreen
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        println!("Application ready, closing splashscreen");
+        splashscreen.close().unwrap();
+    }
+    let main_window = window.get_window("main").unwrap();
+    // Show main window
+    main_window.show().unwrap();
+    main_window.set_focus().unwrap();
 }
 
 fn main() {
-	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![close_splashscreen])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![close_splashscreen])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
